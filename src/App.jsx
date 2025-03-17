9d170431c9e142b7d45c3bed08bab211434ec5ce
Ульяна <panfilovaulyana2016@mail.ru>
2025-03-17 12:35:56 +0300

    1.1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MovieList from "./Components/MovieList.jsx";
+
+function App() {
+
+  return (
+    <section className='MovieApp'>
+      <MovieList/>
+    </section>
+  )
+}
+
+export default App
---
+++ src/Components/MovieList.jsx
@@ -0,0 +1,13 @@
+import './MovieList.css'
+import Movie from './Movie.jsx'
+
+
+export default function MovieList() {
+
+
+    return (
+        <ul className="movie_list">
+             <Movie />
+        </ul>
+    )
+}
